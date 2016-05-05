a3b3d420155337aab452e2ac7a571c740848cfef
Ryan Kim <Ryanfsdf@v1022-rn-195-84.campus-dynamic.uwaterloo.ca>
2016-05-05 14:20:39 -0400

    Added functional contact section

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \nMessage: $message";
+$recipient = "galaxyfsdf@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "I'll get back to you within 24 hours!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
