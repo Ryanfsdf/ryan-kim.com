cbb87b2587a0289bb14a84e01152939a718cd078
Ryan Kim <Ryanfsdf@v1022-rn-195-84.campus-dynamic.uwaterloo.ca>
2016-05-05 14:32:49 -0400

    Fixed PHP file

--- mail.php
+++ mail.php
@@ -6,5 +6,5 @@ $recipient = "galaxyfsdf@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "I'll get back to you within 24 hours!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "I'll get back to you within 24 hours!" . " -" . "<a href='/' style='text-decoration:none;color:blue;'> Return Home</a>";
 ?>
